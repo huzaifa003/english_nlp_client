67da6b501def82d134a58f6006ce211200ea4955
Huzaifa Imran <51008597+huzaifa003@users.noreply.github.com>
2023-08-31 17:02:27 +0000

    Minor Bugs

--- nlp_client_english/src/App.jsx
+++ nlp_client_english/src/App.jsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import QuranicVerses from './QuranicVerses'; // Path to your component
 
-function App() {
-  const [count, setCount] = useState(0)
+import axios from 'axios';
+import React, { useState } from 'react';
 
+const App = () => {
+  const [query, setQuery] = useState("")
+  const ask = async () => {
+    setQuery(document.getElementById("query").value);
+
+    const response = await axios.post("https://flaskdemodeploytrial001.azurewebsites.net/", query);
+    console.log(response);
+  }
   return (
-    <>
+    <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <input type='text' id='query'></input>
+        <button onClick={ask}> Ask Question </button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
---
+++ nlp_client_english/src/QuranicVerses.jsx
@@ -0,0 +1,26 @@
+// import React from 'react';
+// import { Card, Container } from 'react-bootstrap';
+
+// const QuranicVerses = ({ answers }) => {
+//   return (
+//     <Container className="mt-5">
+//       <h1 className="text-center">Quranic Verses</h1>
+//       <div className="row mt-4">
+//         <div className="col-md-8 offset-md-2">
+//           {answers.map((answer, index) => (
+//             <Card key={index} className="mb-3">
+//               <Card.Body>
+//                 <Card.Title>Verse: {answer.meta.SurahNameU} {answer.meta.AyaNo}</Card.Title>
+//                 <Card.Text><strong>Context:</strong> {answer.context}</Card.Text>
+//                 <Card.Text><strong>Answer:</strong> {answer.answer}</Card.Text>
+//                 <Card.Text><strong>Translator:</strong> {answer.meta.translator}</Card.Text>
+//               </Card.Body>
+//             </Card>
+//           ))}
+//         </div>
+//       </div>
+//     </Container>
+//   );
+// };
+
+// export default QuranicVerses;
